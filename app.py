236a23b51deaaca5562126716f44dc2a96d05dab
horlakay <olagunjukayode66@yahoo.com>
2026-03-02 11:37:44 +0100

    code to connect to vertex ai sdk

---
+++ app.py
@@ -0,0 +1,11 @@
+from google import genai
+from google.genai.types import HttpOptions, Part
+
+client = genai.Client(vertexai=True, project='YOUR_PROJECT_ID', location='us-central1')
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash',
+    contents=['What is shown in this image?', 
+              Part.from_uri(file_uri='gs://cloud-samples-data/generative-ai/image/scones.jpg', mime_type='image/jpeg')]
+)
+print(response.text)
